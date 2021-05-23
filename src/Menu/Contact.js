0a74b0af841141a2df1a106c05f7c3561ac596c6
nitu vaidya <vaidyamenitu@gmail.com>
2021-05-23 18:34:06 +0545

    Modified Contact Display

--- src/Menu/Contact.js
+++ src/Menu/Contact.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Title from './Title';
-import {Card} from 'react-bootstrap';
+import {Card, ListGroup} from 'react-bootstrap';
 import {HouseFill,Linkedin,EnvelopeFill} from 'react-bootstrap-icons';
 
 class Contact extends React.Component{
@@ -17,13 +17,17 @@ class Contact extends React.Component{
             <Title heading={window.location.pathname} subheader="Get in Touch"/>
             <div className="row">
               <div className="col-sm">
-            <Card>
-                <Card.Body>
-                    <EnvelopeFill/>{' '}vaidyamenitu@gmail.com<br/>
-                    <Linkedin/>{' '}<a href='http://www.linkedin.com/in/nituvaidya'>linkedin.com/in/nituvaidya</a><br/>
-                    <HouseFill/>{' '}Patan, Lalitpur<br/>
-                </Card.Body>
-            </Card>
+                <Card className='mycard'>
+                    <Card.Header className='mycardheader'></Card.Header>
+                    <Card.Body >
+                    <ListGroup variant="flush" >
+                        <ListGroup.Item className="borderless-list-group-item"> <EnvelopeFill/>{' '}vaidyamenitu@gmail.com<br/></ListGroup.Item>  
+                        <ListGroup.Item className="borderless-list-group-item"><Linkedin/>{' '}<a href='http://www.linkedin.com/in/nituvaidya'>linkedin.com/in/nituvaidya</a><br/>
+</ListGroup.Item>    
+                        <ListGroup.Item className="borderless-list-group-item"><HouseFill/>{' '}Patan, Lalitpur<br/> </ListGroup.Item>                         
+                    </ListGroup>
+                    </Card.Body>
+                </Card>            
             </div>
             <div className="col-sm"></div>
             </div>
