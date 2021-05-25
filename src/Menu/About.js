7d13ede355ede6620f07029cda5beec2797d45a4
nitu vaidya <vaidyamenitu@gmail.com>
2021-05-25 22:34:33 +0545

    Added image and modified display for about page

--- src/Menu/About.js
+++ src/Menu/About.js
@@ -1,24 +1,28 @@
 import React from 'react';
-import logo from '../logo.svg';
+// import logo from '../logo.svg';
+
 
 class About extends React.Component{
     
         render(){
         return( 
-            <div className="container" style={{marginTop:100}}>
-            <div className="row">
-              <div className="col-sm">
-                
-                <img src={logo} className="App-logo" alt="logo" />
-              </div>
-              <div className="col-sm">
-                <span style={{color:'grey'}}>Software Developer</span>
+          
+          <div className="row flex-v-align">
+          <div className='col-sm-5'>
+            <img src='./mpic.jpg' alt='logo' className='photo'/>
+          </div>
+            <div className='col-sm-7'>
+              <div className="home-text hp-left">
+                <span style={{color:'grey'}}>Software Developer</span><br/>
                 <h2>Nitu Vaidya</h2>
-                Motivated Software Engineer with 4+ years of experience; constantly looking forward to work in a challenging environment on cutting edge technologies and to contribute meaningfully towards the organizational and personal growth. Well-versed in technology and writing code to create systems that are reliable and user-friendly.
-              </div>              
+                Motivated Software Engineer with 4+ years of experience;
+                constantly looking forward to work in a challenging environment 
+                on cutting edge technologies and to contribute meaningfully towards 
+                the organizational and personal growth. Well-versed in technology and 
+                writing code to create systems that are reliable and user-friendly.                                                          
+              </div>
             </div>
           </div>
-        
         );
     }
 }
--- src/Menu/Projects.js
+++ src/Menu/Projects.js
@@ -2,18 +2,13 @@ import React from 'react';
 import Title from './Title';
 
 class Projects extends React.Component{
-    constructor(props){
-        super(props);
-        this.state={
-            name:null,
-        };
-    }
-
+    
     render(){
+       
         return( 
-            <div>
-            <Title heading={window.location.pathname}/>
-            My Projects
+        <div >
+            <Title heading={window.location.pathname}/>            
+          
         </div>
         );
     }
